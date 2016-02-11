6fe1537d0f7b3b17eba6fdba548d3f6e0f2bce89
Nick Silvester <nicksil@gmail.com>
2016-02-11 15:25:35 -0500

    Started 'eve' models

--- trader/eve/models.py
+++ trader/eve/models.py
@@ -2,4 +2,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=254, null=False)
+
+    def __unicode__(self):
+        return '{}: {}'.format(self.name, self.id)
+
+
+class SolarSystem(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=254, null=True)
+    region = models.ForeignKey(Region, related_name='solar_systems')
+
+    def __unicode__(self):
+        return '{}: {}'.format(self.name, self.id)
+
+
+class MarketType(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=254, null=True)
+
+    def __unicode__(self):
+        return '{}: {}'.format(self.name, self.id)
