f533cef3343f5c1a431b28d36fc21ab35abbe783
Nick Silvester <nicksil@gmail.com>
2016-02-29 17:04:32 -0500

    Before refactor

--- trader/finder/forms.py
+++ trader/finder/forms.py
@@ -1,7 +1,30 @@
 from django import forms
 
+from eve.models import Region
+
 
 class SingleTypeLookupForm(forms.Form):
 
+    BUY_OR_SELL = (
+        ('buy', 'Buy'),
+        ('sell', 'Sell'),
+    )
+
     region = forms.CharField(label='Region', max_length=256)
     type_name = forms.CharField(label='Market Type', max_length=256)
+    buy_or_sell = forms.ChoiceField(widget=forms.RadioSelect, choices=BUY_OR_SELL)
+
+
+class InterRegionLookupForm(forms.Form):
+
+    regions = Region.objects.all()
+    r = []
+
+    for reg in regions:
+        r.append((reg.id, reg.name))
+
+    REGIONS = r
+
+    origin = forms.ChoiceField(choices=REGIONS)
+    dest = forms.ChoiceField(choices=REGIONS)
+    market_group = forms.IntegerField()
--- trader/finder/views.py
+++ trader/finder/views.py
@@ -1,7 +1,15 @@
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView
 
+import requests
+
+from eve.models import MarketType
+from eve.models import Region
+
+from finder.forms import InterRegionLookupForm
 from finder.forms import SingleTypeLookupForm
+
+CREST_ORDER_URL = 'https://public-crest.eveonline.com/market/{region_id}/orders/{buy_or_sell}/?type=https://public-crest.eveonline.com/types/{type_id}/'
 
 
 class SingleTypeLookupView(FormView):
@@ -10,10 +18,54 @@ class SingleTypeLookupView(FormView):
     template_name = 'finder/single_type_lookup_form.html'
     success_url = reverse_lazy('finder:single_type_lookup_view')
 
-    def post(self, request, *args, **kwargs):
-        form = self.get_form()
+    def form_valid(self, form):
+        region = form.cleaned_data['region']
+        type_name = form.cleaned_data['type_name']
+        buy_or_sell = form.cleaned_data['buy_or_sell']
 
-        if form.is_valid():
-            return self.form_valid(form)
-        else:
-            return self.form_invalid(form)
+        region_obj = Region.objects.get(name__iexact=region)
+        type_obj = MarketType.objects.get(name__iexact=type_name)
+
+        region_id = region_obj.id
+        type_id = type_obj.id
+
+        url = CREST_ORDER_URL.format(region_id=region_id, buy_or_sell=buy_or_sell, type_id=type_id)
+
+        return super(SingleTypeLookupView, self).form_valid(form)
+
+
+class InterRegionLookupView(FormView):
+
+    form_class = InterRegionLookupForm
+    template_name = 'finder/inter_region_lookup_form.html'
+    success_url = reverse_lazy('finder:inter_region_lookup_view')
+
+    def __init__(self, **kwargs):
+        self.crest_data = {
+            'origin': [],
+        }
+        super(InterRegionLookupView, self).__init__(**kwargs)
+
+    def form_valid(self, form):
+
+        origin_id = int(form.cleaned_data['origin'])
+        dest_id = int(form.cleaned_data['origin'])
+        market_group = form.cleaned_data['market_group']
+
+        market_types = MarketType.objects.filter(market_group=market_group)
+
+        for t in market_types:
+            url = CREST_ORDER_URL.format(region_id=origin_id, buy_or_sell='sell', type_id=t.id)
+            r = requests.get(url)
+            r_json = r.json()
+
+            for order in r_json['items']:
+                _ = {
+                    'price': order['price'],
+                    'volume': order['volume'],
+                    'station': order['location']['name'],
+                    'type_name': order['type']['name'],
+                }
+                self.crest_data['origin'].append(_)
+
+        return super(InterRegionLookupView, self).form_valid(form)
